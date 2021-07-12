88fc94e87f56529b3711e7546198997006f2fbf9
Pakistaninqq <83083809+Pakistaninqq@users.noreply.github.com>
2021-07-12 23:11:53 +0300

    Add files via upload

---
+++ src/main/java/me/cumhax/chipshack/util/EntityUtil.java
@@ -0,0 +1,9 @@
+package me.cumhax.chipshack.util;
+
+import net.minecraft.entity.Entity;
+
+public class EntityUtil {
+    public static boolean isValidEntity(Entity ent, Float value){
+        return false;
+    }
+}
---
+++ src/main/java/me/cumhax/chipshack/util/HudUtil.java
@@ -0,0 +1,9 @@
+package me.cumhax.chipshack.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.Minecraft;
+
+public class HudUtil {
+
+}
